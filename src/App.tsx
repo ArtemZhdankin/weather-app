68c7eabae1047eb45d229d187a4581566b927d19
ArtemZhdankin <106024923+ArtemZhdankin@users.noreply.github.com>
2023-10-02 20:30:31 -0400

    feat: weather app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import Button from 'react-bootstrap/Button';
-import WeatherData from './interfaces/weather-data.interface';
+import WeatherData, { WeatherForecastSummary } from './interfaces/weather-data.interface';
+import './App.css'
+import {format} from 'date-fns'
+import classNames from 'classnames';
+import { Button } from 'react-bootstrap';
 
-
-import Card from 'react-bootstrap/Card';
 
 const App = () => {
 
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null)
+  const [activeDay, setActiveDay] = useState<WeatherForecastSummary | null>(null)
+  const [searchValue, setSearchValue] = useState<string>('')
+
+  const dayProperties = [
+    {title: 'Temperature, C', key: 'temp_c'},
+    {title: 'Wind, Kph', key: 'wind_kph'},
+    {title: 'Chance of rain, %', key: 'chance_of_rain'},
+    {title: 'Feels like, C', key: 'feelslike_c'},
+    {title: 'Wind direction', key: 'wind_dir'},
+    {title: 'Dewpoint, C', key: 'dewpoint_c'},
+    {title: 'UV Index', key: 'uv'}
+  ]
+
+  const fetchData = async (query: string) => {
+    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=f9ba2df78d6841d7b25214748230210&q=${query}&days=7&aqi=yes`, {method:'get'})
+  
+    if (response.ok) {
+      const data = await response.json() as WeatherData
+      console.log(data)
+
+      setWeatherData(data)
+      setActiveDay(data.forecast.forecastday[0])
+    }
+  }
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -16,32 +40,76 @@ const App = () => {
       const lat = position.coords.latitude
       const lon = position.coords.longitude
 
-      const fetchData = async () => {
-        const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=f9ba2df78d6841d7b25214748230210&q=${lat},${lon}&days=7`, {method:'get'})
-      
-        if (response.ok) {
-          const data = await response.json()
-          console.log(data)
-
-          setWeatherData(data)
-        }
-      }
-      
-      fetchData()
+      fetchData(`${lat},${lon}`)
         
     });
   }, [])
   
   return (
     <div className="App">
-      <h3>Weather in {weatherData?.location.name}, {weatherData?.location.region}, {weatherData?.location.country}</h3>
+      <a className='logo' href="https://www.weatherapi.com/" title="Free Weather API">
+        <img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" />
+      </a>
+
+      <div className='header__search'>
+        <input placeholder='City name..' value={searchValue} onChange={(e) => { setSearchValue(e.target.value)} } type='text' />
+        <Button variant='primary' onClick={() => fetchData(searchValue)}>Search</Button>
+      </div>
+
+      <h3 className='header'>Weather in {weatherData?.location.name}, {weatherData?.location.region}, {weatherData?.location.country}</h3>
 
       <div>
         <img width={64} height={64} src={weatherData?.current.condition.icon} /> 
+        <b>{weatherData?.current.condition.text}</b>&nbsp;|
         Temperature: <b>{weatherData?.current.temp_c} C</b>&nbsp;|
         Wind: <b>{weatherData?.current.wind_mph} {weatherData?.current.wind_dir}</b>&nbsp;|
         Pressure: <b>{weatherData?.current.pressure_mb} Mb</b>&nbsp;|
         Feels like: <b>{weatherData?.current.feelslike_c} C</b>
+      </div>
+
+      <div className='forecast'>
+        {weatherData?.forecast.forecastday.map((forecastDay, index) => {
+            return <div className={classNames('forecast__day', { 'forecast__day--active': forecastDay === activeDay })} key={index} onClick={() => setActiveDay(forecastDay)}>
+              <b className='forecast__day-title'>{format(new Date(forecastDay.date + ' 12:00:00'), 'MMM, do')}</b><br/>
+              <span>{format(new Date(forecastDay.date + ' 12:00:00'), 'EEEE')}</span><br/>
+              <img width={64} height={64} src={forecastDay.day.condition.icon} /> <br/>
+              <div className='forecast__day-desc'>{forecastDay.day.condition.text}</div>
+              <div><span className='forecast__day-min'>{forecastDay.day.mintemp_c}C</span> ... <span className='forecast__day-max'>{forecastDay.day.maxtemp_c}C</span></div>
+            </div>
+          })
+        }
+      </div>
+
+      <div className='forecast-details'>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              {activeDay?.hour.map((hour, index) => {
+                return <th key={index}>{format(new Date(hour.time), 'hhaaa')}</th>
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              {activeDay?.hour.map((hour, index) => {
+                  return <td key={index}><img width={32} height={32} src={hour.condition.icon} /> </td>
+                })}
+            </tr>
+
+            {dayProperties.map((prop, index) => {
+              return <tr key={index}>
+                      <td><b>{prop.title}</b></td>
+                      {activeDay?.hour.map((hour, index) => {
+                          return <td key={index}>{(hour as Record<string, any>)[prop.key]}</td>
+                        })}
+                    </tr>
+            })}
+          </tbody>
+        </table>
+
+        
       </div>
     </div>
   );
--- src/interfaces/weather-data.interface.ts
+++ src/interfaces/weather-data.interface.ts
@@ -20,7 +20,62 @@ export interface WeatherLocation {
     country: string;
 }
 
+export interface WeatherForecast {
+    forecastday: WeatherForecastSummary[]
+}
+
+export interface WeatherForecastSummary {
+    date: string
+    date_epoch: number;
+    day: WeatherForecastDay
+    hour: WeatherForecastHour[]
+}
+
+export interface WeatherForecastHour {
+    time: string
+    time_epoch: number;
+    condition: WeatherCondition;
+    temp_c: number;
+    temp_f: number;
+    wind_mph: number;
+    wind_kph: number;
+    wind_degree: number;
+    wind_dir: string;
+    pressure_mb: number;
+    pressure_in: number;
+    precip_mm: number;
+    precip_in: number;
+    humidity: number;
+    cloud: number;
+    feelslike_c: number;
+    feelslike_f: number;
+    windchill_c: number;
+    windchill_f: number;
+    heatindex_c: number;
+    heatindex_f: number;
+    dewpoint_c: number;
+    dewpoint_f: number;
+    will_it_rain: number;
+    chance_of_rain: number;
+    will_it_snow: number;
+    chance_of_snow: number;
+    vis_km: number;
+    vis_miles: number;
+    gust_mph: number;
+    gust_kph: number;
+    uv: number;
+}
+
+export interface WeatherForecastDay {
+    condition: WeatherCondition
+    mintemp_c: number
+    mintemp_f: number
+    maxtemp_c: number
+    maxtemp_f: number 
+}
+
 export default interface WeatherData {
     current: CurrentWeatherData;
     location: WeatherLocation;
+    forecast: WeatherForecast
 }
