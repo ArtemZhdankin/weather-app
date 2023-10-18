631e26988bda64f5035c3cee2e7ada15baaf3b2e
ArtemZhdankin <106024923+ArtemZhdankin@users.noreply.github.com>
2023-10-18 19:17:35 -0400

    feat: more data

--- src/App.tsx
+++ src/App.tsx
@@ -32,17 +32,32 @@ const App = () => {
 
   const dayProperties = [
     {label: 'Temperature, C', value: 'temp_c'},
-    {label: 'Wind, Kph', value: 'wind_kph'},
+    {label: 'Heat Index, C', value: 'heatindex_c'},
+    {label: 'Pressure, MB', value: 'pressure_mb'},
+    {label: 'Humidity, %', value: 'humidity'},
+    {label: 'Cloud Ceiling', value: 'cloud'},
+    {label: 'Visibility, Km', value: 'vis_km'},
+    {label: 'Chance of snow, %', value: 'chance_of_snow'},
     {label: 'Chance of rain, %', value: 'chance_of_rain'},
+    {label: 'Wind, Kph', value: 'wind_kph'},
+    {label: 'Wind Gust, Kph', value: 'gust_kph'},
     {label: 'Feels like, C', value: 'feelslike_c'},
     {label: 'Wind direction', value: 'wind_dir'},
+    {label: 'Wind chill, C', value: 'windchill_c'},
     {label: 'Dewpoint, C', value: 'dewpoint_c'},
     {label: 'UV Index', value: 'uv'}
   ]
 
   const [selectedDayProperties, setSelectedDayProperties] = useState([
     {label: 'Temperature, C', value: 'temp_c'},
-    {label: 'Wind, Kph', value: 'wind_kph'}]
+    {label: 'Feels like, C', value: 'feelslike_c'},
+    {label: 'Pressure, MB', value: 'pressure_mb'},
+    {label: 'Humidity, %', value: 'humidity'},
+    {label: 'Chance of rain, %', value: 'chance_of_rain'},
+    {label: 'Visibility, Km', value: 'vis_km'},
+    {label: 'Wind, Kph', value: 'wind_kph'},
+    {label: 'Wind direction', value: 'wind_dir'},
+    {label: 'UV Index', value: 'uv'}]
   )
 
   const handleSearch = (query: string) => {
